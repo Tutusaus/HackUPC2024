59174d993b1d7763f217c268abc41188d0247258
Tutusaus <guillemtutusaus@gmail.com>
2024-05-04 20:53:46 +0200

    finally managed to complete the algorithm

--- main.cpp
+++ main.cpp
@@ -32,224 +32,7 @@ void recorrerArbre(Node *pare){
     for(int Fill=0;Fill<pare->n_fills;Fill++){
         recorrerArbre(pare->fills[(pare->n_fills)-(Fill+1)]);
     }
-    imprimirNode(pare);
-}
-
-void copiaTauler(char t1[N][N],char t2[N][N]){  //copia el tauler del node 1 al node 2
-    for(int i=0;i<N;i++){
-        for(int j=0;j<N;j++){
-            t2[i][j]=t1[i][j];
-        }
-    }
-}
-int calculaColumna(char t[N][N],int fill_index){
-    int index=0;
-    for(int i=0;i<N;i++){
-        if(t[0][i]=='0'){
-            if(fill_index==index){
-                return i;
-            }
-            index++;
-        }
-    }
-}
-int calculaFila(char t[N][N],int col){
-    for(int i=0;i<N;i++){
-        if(t[(N-1)-i][col]=='0'){
-            return (N-1)-i;
-        }
-    }
-}
-bool quatrenratlla(char t[N][N], int fila, int col) {
-    for(int j=-1;j<2;j++) {
-        for(int i=-1;i<2;i++) {
-            if(i!=0 or j!=0) {
-                if(t[fila][col] == t[fila+j][col+i]) {
-                    int direcx = i-fila;
-                    int direcy = j-col;
-                    //cout << tauler[fila][col] << tauler[i][j] << tauler[i+direcx][j+direcy] << tauler[i+2*direcx][j+2*direcy] << endl;
-                    if(t[fila][col]==t[i+direcx][j+direcy] and t[fila][col]==t[i-direcx][j-direcy]) {
-                        //cout << tauler[fila][col] << tauler[i+direcx][j+direcy] << tauler[i-direcx][j-direcy];
-                        if(t[i+direcx][j+direcy]==t[i+2*direcx][j+2*direcy] or t[i-direcx][j-direcy]==t[i-2*direcx][j-2*direcy]) {
-                            return false;
-                        }
-                    }
-                    if(t[fila][col]==t[i+direcx][j+direcy] and t[fila][col]==t[i+2*direcx][j+2*direcy]) {
-                        return false;
-                    }
-                }
-            }
-        }
-    }
-}
-void aplicarTirada(int nivell,char t[N][N],int fila,int columna){
-    if(nivell%2==1){
-        t[fila][columna]='2';   //PC
-    }
-    else{
-        t[fila][columna]='1';   //Jugador
-    }
-}
-int calcularNumFills(char t[N][N]) {
-    int num_de_cols_plenes=0;
-    for(int i=0;i<N;i++){
-        if(t[0][i]!='0'){
-            num_de_cols_plenes++;
-        }
-    }
-    return N-num_de_cols_plenes;
-}
-Node *crearNode(Node *pare,int fill_index){ // Aquesta funció té com a arguments l'adreça del node pare que la genera, l'índex d'aquest (fill número "tal" del node pare), i nivell on es troba el node (si és 2 llavors aquest node passa a ser un "node fulla")
-    Node *p=new Node;           // Creem l'adreça de memòria que farà referència al primer fill de tots els possibles fills del node pare. Davant duu la paraula Node perquè aquesta adreça contindrà nodes.
-    copiaTauler(pare->tauler,p->tauler);    // Copiem el tauler del node pare al node fill, posteriorment aplicarem la corresponent jugada.
-    p->nivell=(pare->nivell)+1;             // Incrementem el nivell a nivell++ per tots els fills del node pare.
-    int col=calculaColumna(p->tauler,fill_index); // Calculem la columna on s'aplicarà la següent jugada.
-    int fila=calculaFila(p->tauler,col);          // Calculem la fila on s'aplicarà la següent jugada.
-    aplicarTirada(p->nivell,p->tauler,fila,col);  // Un cop calculada la fila i la columna on caldrà aplicar la següent jugada, modifiquen el tauler amb la fitxa corresponent
-
-    if(quatrenratlla(p->tauler, fila, col)) {
-        p->n_fills=0;
-        p->fills=nullptr;
-        if(p->nivell%2==0) {
-            p->valor=-INFTY;
-        }
-        else {
-            p->valor=INFTY;
-        }
-    }
-    else {
-        p->valor=0;
-        p->n_fills=calcularNumFills(p->tauler);
-        p->fills= new Node *[p->n_fills];
-    }
-    return p;
-}
-void dibuixarTauler(char tauler[N][N], int col) {
-    int topLeft = 201;
-    int topRight = 187;
-    int botLeft = 200;
-    int botRight = 188;
-    int vLeft = 185;
-    int vRight = 204;
-    int hBot = 203;
-    int hTop = 202;
-    int Horizontal = 205;
-    int Vertical = 186;
-    int cross = 206;
-    int space = 32;
-    string X = " X ";
-    string O = " O ";
-
-
-    cout << char(topLeft);
-    for (int i = 0; i < 7; i++) {
-        for (int i = 0; i < 3; i++) {
-            cout << char(Horizontal);
-        }
-        cout << char(hBot);
-    }
-    cout << char(Horizontal) << char(Horizontal) << char(Horizontal) << char(topRight) << endl;
-
-
-    for (int i = 0; i < 8; i++) {
-
-        for (int j = 0; j < 8; j++) {
-            cout << char(Vertical);
-            if (tauler[i][j] == '1') {
-                cout << X;
-            } else if (tauler[i][j] == '2') {
-                cout << O;
-            } else {
-                for (int k = 0; k < 3; k++) {
-                    cout << char(space);
-                }
-            }
-        }
-        cout << char(Vertical) << endl;
-
-        if (i < 7) {
-            cout << char(vRight);
-            for (int i = 0; i < 7; i++) {
-                for (int i = 0; i < 3; i++) {
-                    cout << char(Horizontal);
-                }
-                cout << char(cross);
-            }
-            cout << char(Horizontal) << char(Horizontal) << char(Horizontal) << char(vLeft) << endl;
-        }
-    }
-
-    cout << char(botLeft);
-    for (int i = 0; i < 7; i++) {
-        for (int i = 0; i < 3; i++) {
-            cout << char(Horizontal);
-        }
-        cout << char(hTop);
-    }
-    cout << char(Horizontal) << char(Horizontal) << char(Horizontal) << char(botRight) << endl;
-    cout << "  ";
-    for(int i=0;i<N;i++) {
-        cout << i+1 << "   ";
-    }
-    cout << endl;
-}
-void inicialitzarTauler (char tauler[N][N]){
-    for (int fila = 0; fila < N ;fila++){
-        for(int col = 0; col<N ; col++){
-            tauler[fila][col] = '0';
-        }
-    }
-
-}
-void imprimirTauler(char tauler[N][N]){
-    for (int fila = 0; fila < N ;fila++){
-        for(int col = 0; col<N ; col++){
-            cout<< tauler[fila][col];
-        }
-        cout<< endl;
-    }
-}
-void modificarTauler(char tauler[N][N], int col, int ronda) {
-    for (int i=N-1; i>=0; i--){
-        if(tauler[i][col] == '0'){
-            if(ronda%2!=0){
-                tauler[i][col] = '1';
-            }
-            else {
-                tauler[i][col] = '2';
-            }
-            break;
-        }
-    }
-}
-void checkD(char tauler[N][N], int fila, int col, int i, int j, bool *run) {
-    int direcx = i-fila;
-    int direcy = j-col;
-    //cout << tauler[fila][col] << tauler[i][j] << tauler[i+direcx][j+direcy] << tauler[i+2*direcx][j+2*direcy] << endl;
-    if(tauler[fila][col]==tauler[i+direcx][j+direcy] and tauler[fila][col]==tauler[i-direcx][j-direcy]) {
-        //cout << tauler[fila][col] << tauler[i+direcx][j+direcy] << tauler[i-direcx][j-direcy];
-        if(tauler[i+direcx][j+direcy]==tauler[i+2*direcx][j+2*direcy] or tauler[i-direcx][j-direcy]==tauler[i-2*direcx][j-2*direcy]) {
-            *run = false;
-        }
-    }
-    if(tauler[fila][col]==tauler[i+direcx][j+direcy] and tauler[fila][col]==tauler[i+2*direcx][j+2*direcy]) {
-       *run = false;
-    }
-}
-void check(char tauler[N][N], int col, bool *run) {
-    int fila=0;
-    while(tauler[fila][col] == '0') {
-        fila++;
-    }
-    for(int j=-1;j<2;j++) {
-        for(int i=-1;i<2;i++) {
-            if(i!=0 or j!=0) {
-                if(tauler[fila][col] == tauler[fila+j][col+i]) {
-                    checkD(tauler, fila, col, fila+j, col+i, run);
-                }
-            }
-        }
-    }
+    //imprimirNode(pare);
 }
 void    checkS(char t[N][N],int a,int b,int *i){
     if(t[a][b]==t[a+1][b] && a<(N-1)){
@@ -549,6 +332,96 @@ int heuristica(Node *node,int pes_3ratlla/* ,int pes_2ratlla,int pes_1ratlla */)
         return assignavalor(node,pes_3ratlla*Enratlla3(node)/* +pes_2ratlla*Enratlla2(p)+pes_1ratlla*Enratlla1(p) */);
     }
 }
+void copiaTauler(char t1[N][N],char t2[N][N]){  //copia el tauler del node 1 al node 2
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            t2[i][j]=t1[i][j];
+        }
+    }
+}
+int calcularNumFills(char t[N][N]){
+    int num_de_cols_plenes=0;
+    for(int i=0;i<N;i++){
+        if(t[0][i]!='0'){
+            num_de_cols_plenes++;
+        }
+    }
+    return N-num_de_cols_plenes;
+}
+int calculaColumna(char t[N][N],int fill_index){
+    int index=0;
+    for(int i=0;i<N;i++){
+        if(t[0][i]=='0'){
+            if(fill_index==index){
+                return i;
+            }
+            index++;
+        }
+    }
+}
+int calculaFila(char t[N][N],int col){
+    for(int i=0;i<N;i++){
+        if(t[(N-1)-i][col]=='0'){
+            return (N-1)-i;
+        }
+    }
+}
+void aplicarTirada(int nivell,char t[N][N],int fila,int columna){
+    if(nivell%2==1){
+        t[fila][columna]='2';   //PC
+    }
+    else{
+        t[fila][columna]='1';   //Jugador
+    }
+}
+bool quatrenratlla(char t[N][N], int fila, int col) {
+    for(int j=-1;j<2;j++) {
+        for(int i=-1;i<2;i++) {
+            if(i!=0 or j!=0) {
+                if(t[fila][col] == t[fila+j][col+i]) {
+                    int direcx = i-fila;
+                    int direcy = j-col;
+                    //cout << tauler[fila][col] << tauler[i][j] << tauler[i+direcx][j+direcy] << tauler[i+2*direcx][j+2*direcy] << endl;
+                    if(t[fila][col]==t[i+direcx][j+direcy] and t[fila][col]==t[i-direcx][j-direcy]) {
+                        //cout << tauler[fila][col] << tauler[i+direcx][j+direcy] << tauler[i-direcx][j-direcy];
+                        if(t[i+direcx][j+direcy]==t[i+2*direcx][j+2*direcy] or t[i-direcx][j-direcy]==t[i-2*direcx][j-2*direcy]) {
+                            return false;
+                        }
+                    }
+                    if(t[fila][col]==t[i+direcx][j+direcy] and t[fila][col]==t[i+2*direcx][j+2*direcy]) {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+Node *crearNode(Node *pare,int fill_index){ // Aquesta funció té com a arguments l'adreça del node pare que la genera, l'índex d'aquest (fill número "tal" del node pare), i nivell on es troba el node (si és 2 llavors aquest node passa a ser un "node fulla")
+    Node *p=new Node;           // Creem l'adreça de memòria que farà referència al primer fill de tots els possibles fills del node pare. Davant duu la paraula Node perquè aquesta adreça contindrà nodes.
+    copiaTauler(pare->tauler,p->tauler);    // Copiem el tauler del node pare al node fill, posteriorment aplicarem la corresponent jugada.
+    p->nivell=(pare->nivell)+1;             // Incrementem el nivell a nivell++ per tots els fills del node pare.
+    int col=calculaColumna(p->tauler,fill_index); // Calculem la columna on s'aplicarà la següent jugada.
+    int fila=calculaFila(p->tauler,col);          // Calculem la fila on s'aplicarà la següent jugada.
+    aplicarTirada(p->nivell,p->tauler,fila,col);  // Un cop calculada la fila i la columna on caldrà aplicar la següent jugada, modifiquen el tauler amb la fitxa corresponent
+
+    if(checkFila(p->tauler,fila,col,4) || checkCol(p->tauler,fila,col,4) || checkDiag(p->tauler,fila,col,4)) {
+        p->n_fills=0;
+        p->fills=nullptr;
+        if(p->nivell%2==0) {
+            p->valor=-INFTY;
+        }
+        else {
+            p->valor=INFTY;
+        }
+    }
+    else {
+        p->valor=0;
+        p->n_fills=calcularNumFills(p->tauler);
+        p->fills= new Node *[p->n_fills];
+    }
+    return p;
+}
 void crearNivell(Node *pare){
     for(int Fill=0;Fill<pare->n_fills;Fill++){
         pare->fills[Fill]=crearNode(pare,Fill);
@@ -564,7 +437,7 @@ void crearArbre(Node *pare, int profunditat){
     else{
         //Node fulla
         pare->n_fills=0;
-        pare->fills=NULL;
+        pare->fills=nullptr;
         pare->valor=heuristica(pare,100);
     }
 }
@@ -604,7 +477,55 @@ void minimax(Node *pare){
         }
     }
 }
+void dibuixarTauler(char t[N][N]){
+    printf("%c%c%c",201,205,205);
 
+    for(int i=0;i<(N-1);i++){
+        printf("%c%c%c%c",205,203,205,205);
+    }
+    printf("%c%c\n",205,187);
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            switch(t[i][j]){
+                case '1':
+                    printf("%c%c%c%c",186,255,79,255);
+                    break;
+
+                case '2':
+                    printf("%c%c%c%c",186,255,88,255);
+                    break;
+
+                default:
+                    printf("%c%c%c%c",186,255,255,255);
+                    break;
+            }
+        }
+
+        printf("%c\n",186);
+
+        if(i==(N-1)){
+            printf("%c%c%c",200,205,205);
+            for(int i=0;i<(N-1);i++){
+                printf("%c%c%c%c",205,202,205,205);
+            }
+            printf("%c%c\n",205,188);
+        }else{
+            printf("%c%c%c",204,205,205);
+            for(int i=0;i<(N-1);i++){
+                printf("%c%c%c%c",205,206,205,205);
+            }
+            printf("%c%c\n",205,185);
+        }
+    }
+
+    printf("%c%c",255,255);
+    for(int i=1;i<(N+1);i++){
+        printf("%d%c%c%c",i,255,255,255);
+    }
+    printf("%c%c",255,255);
+    printf("\n");
+}
 int calculacol(Node *pare){
     for(int Fill=0;Fill<pare->n_fills;Fill++){
         if((pare->fills[Fill])->valor==pare->valor){
@@ -619,7 +540,6 @@ int calculacol(Node *pare){
         }
     }
 }
-
 void esborrarArbre(Node *pare){
     for(int Fill=0;Fill<pare->n_fills;Fill++){
         if((pare->fills[Fill])->n_fills!=0){
@@ -630,7 +550,64 @@ void esborrarArbre(Node *pare){
         }
     }
 }
+void inicialitzarTauler (char tauler[N][N]){
+    for (int fila = 0; fila < N ;fila++){
+        for(int col = 0; col<N ; col++){
+            tauler[fila][col] = '0';
+        }
+    }
 
+}
+void imprimirTauler(char tauler[N][N]){
+    for (int fila = 0; fila < N ;fila++){
+        for(int col = 0; col<N ; col++){
+            cout<< tauler[fila][col];
+        }
+        cout<< endl;
+    }
+}
+void modificarTauler(char tauler[N][N], int col, int ronda) {
+    for (int i=N-1; i>=0; i--){
+        if(tauler[i][col] == '0'){
+            if(ronda%2!=0){
+                tauler[i][col] = '1';
+            }
+            else {
+                tauler[i][col] = '2';
+            }
+            break;
+        }
+    }
+}
+void checkD(char tauler[N][N], int fila, int col, int i, int j, bool *run) {
+    int direcx = i-fila;
+    int direcy = j-col;
+    //cout << tauler[fila][col] << tauler[i][j] << tauler[i+direcx][j+direcy] << tauler[i+2*direcx][j+2*direcy] << endl;
+    if(tauler[fila][col]==tauler[i+direcx][j+direcy] and tauler[fila][col]==tauler[i-direcx][j-direcy]) {
+        //cout << tauler[fila][col] << tauler[i+direcx][j+direcy] << tauler[i-direcx][j-direcy];
+        if(tauler[i+direcx][j+direcy]==tauler[i+2*direcx][j+2*direcy] or tauler[i-direcx][j-direcy]==tauler[i-2*direcx][j-2*direcy]) {
+            *run = false;
+        }
+    }
+    if(tauler[fila][col]==tauler[i+direcx][j+direcy] and tauler[fila][col]==tauler[i+2*direcx][j+2*direcy]) {
+       *run = false;
+    }
+}
+void check(char tauler[N][N], int col, bool *run) {
+    int fila=0;
+    while(tauler[fila][col] == '0') {
+        fila++;
+    }
+    for(int j=-1;j<2;j++) {
+        for(int i=-1;i<2;i++) {
+            if(i!=0 or j!=0) {
+                if(tauler[fila][col] == tauler[fila+j][col+i]) {
+                    checkD(tauler, fila, col, fila+j, col+i, run);
+                }
+            }
+        }
+    }
+}
 int main(){
     bool run = true;
     int ronda = 1;
@@ -658,7 +635,7 @@ int main(){
             esborrarArbre(arrel);
         }
         modificarTauler(tauler, col-1, ronda);
-        dibuixarTauler(tauler, col-1);
+        dibuixarTauler(tauler);
         check(tauler, col-1, &run);
         ronda++;
     }
